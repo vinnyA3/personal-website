2713573279df27cf55141c1ff13994f7695a97f9
vinnyA3 <vincent.aceto@gmail.com>
2020-11-05 00:04:18 -0500

    style(gatsby-node): formatting

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,17 @@
+var fs = require("fs-extra")
+var path = require("path")
+
+exports.onPostBuild = () => {
+  // Handle CNAME configuration
+  console.log("Copying CNAME file to /public ...")
+  fs.copyFileSync(
+    path.join(__dirname, "CNAME"),
+    path.join(__dirname, "/public/CNAME")
+  )
+}
+
 /**
  * Implement Gatsby's Node APIs in this file.
  *
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
-
-var fs = require('fs-extra')
-var path = require('path')
-
-exports.postBuild = (pages, callback) => {
-  console.log('Copying CNAME file to /public ...')
-  fs.copyFileSync(path.join(__dirname, 'CNAME'), path.join(__dirname, '/public/CNAME'))
-  callback()
-}
