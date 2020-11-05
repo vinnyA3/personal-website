17c62f1eec071d49d36eda5dc367115145044b4a
vinnyA3 <vincent.aceto@gmail.com>
2020-11-04 22:46:19 -0500

    feat: change gatsby-config, add google fonts

--- vinny-src/gatsby-config.js
+++ vinny-src/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Vincent Aceto`,
+    description: `Vincent Aceto.  A Software Engineer based in NYC.`,
+    author: `@vinnya3`,
   },
   plugins: [
     `gatsby-plugin-sass`,
@@ -17,19 +17,10 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `gatsby-plugin-google-fonts`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        fonts: [`Inconsolata`], // TODO: select font sizes
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
