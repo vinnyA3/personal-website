3e7e764f46f7b0a2c9459880ae1a401fa037a8a1
vinnyA3 <vincent.aceto@gmail.com>
2020-11-06 21:27:11 -0500

    feat(gatsby-config): add module resolver plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,20 @@ module.exports = {
     // `gatsby-transformer-sharp`,
     // `gatsby-plugin-sharp`,
     {
+      resolve: 'gatsby-plugin-module-resolver',
+      options: {
+        root: './src', // <- will be used as a root dir
+        aliases: {
+          '@components': './components',
+          helpers: './helpers',
+          static: {
+            root: './public', // <- will used as this alias' root dir
+            alias: './static' // <- will become ./public/static
+          }
+        }
+      }
+    },
+    {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [`Inconsolata`], // TODO: select font sizes
