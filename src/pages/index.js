49e699650095ea25ad8bde839ec6e242277eaf68
vinnyA3 <vincent.aceto@gmail.com>
2020-12-13 16:07:38 -0500

    feat: remove unnecessary code, formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,34 @@
 import React, { useEffect } from "react"
 import { Link } from "gatsby"
 import SEO from "@components/seo"
-import DarkModeSwitch from '@components/darkmode-switch'
-import VincentProfile from '@assets/images/vincent-profile.jpeg' 
+import DarkModeSwitch from "@components/darkmode-switch"
+import VincentProfile from "@assets/images/vincent-profile.jpeg"
 import "@sass/main.scss"
 import "./styles.scss"
 
 const IndexPage = () => {
   useEffect(() => {
-    // TODO: move this shit out  
-    const gatsbyWrapper = document.getElementById('gatsby-focus-wrapper')
-    gatsbyWrapper.style.height = '100%';
-    gatsbyWrapper.style.width = '100%';
+    if (process.env.IS_DEV) {
+      console.log('RUNNING IN DEV')
+      // const gatsbyWrapper = document.getElementById("gatsby-focus-wrapper")
+      // gatsbyWrapper.style.height = "100%"
+      // gatsbyWrapper.style.width = "100%"
+    }
   }, [])
 
   return (
     <div className="home-wrapper">
       <SEO title="Home" />
-      <DarkModeSwitch style={{ position: 'absolute', top: '5em', right: '5em' }}/>
+      <DarkModeSwitch
+        style={{ position: "absolute", top: "5em", right: "5em" }}
+      />
       <section style={{ textAlign: "center" }}>
         <div className="intro__brand">
-          <img className="intro__avatar" src={VincentProfile} alt="Vincent Aceto. That's me!!" />
+          <img
+            className="intro__avatar"
+            src={VincentProfile}
+            alt="Vincent Aceto. That's me!!"
+          />
           <h4 className="intro__name">Vincent Aceto</h4>
         </div>
         <h1 className="intro__profession">Software Engineer</h1>
