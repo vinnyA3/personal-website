dd5b2395180f49eb8ccfeb3c6318268c9a353a5c
vinnyA3 <vincent.aceto@gmail.com>
2021-10-30 23:15:12 -0400

    feat: add social & rss icon to site home header
    * adds fontawesome things to project
    * adds fa-icons (+ links) & styles
    * misc prettier formatting

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,8 @@
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+config.autoAddCss = false // disable font awesome style injection
+
 import 'styles/global.css';
 
 export default function App({ Component, pageProps }) {
